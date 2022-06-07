ed62f2b63657938e8ade9b9950cfd9c9eb024485
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-07 18:17:07 -0400

    move post format to slice prepare callback

--- src/features/post/AddPostForm.js
+++ src/features/post/AddPostForm.js
@@ -15,14 +15,8 @@ const AddPostForm = () => {
 
     const onSavePostClicked = () => {
         if (title && content) {
-            dispatch(
-                postAdded({
-                    id: nanoid(),
-                    title,
-                    content
-                })
-            );
-
+            dispatch(postAdded(title, content));
+            
             setTitle('');
             setContent('');
         }
